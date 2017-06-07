05c1514921658bb7e9b9716817bfbaa685cea089
Sergey Baigudin <sergey@baigudin.software>
2017-06-07 21:49:29 +0300

    pre-v0.7.1
    SL C8051F9x comparator driver has been added.

--- boos/driver/siliconlabs/c8051f90x/boos.driver.comparator.c
+++ boos/driver/siliconlabs/c8051f90x/boos.driver.comparator.c
@@ -7,6 +7,7 @@
  * @link      http://baigudin.software
  */
 #include "boos.driver.comparator.h" 
+#include "boos.driver.interrupt.h" 
 #include "boos.driver.registers.h"
 #include "boos.driver.constants.h"
 
@@ -31,26 +32,65 @@
 #define RES_INDEX_MASK (0x01)
 
 /**
+ * Comparator interrupt handlers.
+ */  
+typedef struct _Handler
+{
+  /**
+   * The driver interrupt handler.
+   */  
+  void(*drv)();
+  
+  /**
+   * External interrupt handler.
+   */  
+  void(*ext)();  
+  
+} Handler;
+
+/**
+ * Comparator interrupt data.
+ */  
+typedef struct _Interrupt
+{
+  /**
+   * The driver interrupt resource.
+   */  
+  int8 res;  
+  
+  /**
+   * The driver interrupt source.
+   */  
+  int8 source;
+ 
+} Interrupt; 
+
+/**
  * Comparator contex.
  */
 typedef struct _Comparator
 {
   /**
-   * Comparator interrupt resource.
+   * Resource locked flag.  
    */
-  int8 intRes;
+  int8 lock;
+
+  /**
+   * Comparator interrupt handlers.
+   */  
+  Handler handler;  
+
+  /**
+   * Comparator interrupt data.
+   */  
+  Interrupt inter;    
 
 } Comparator;
 
 /**
  * Comparator resource contex.
  */
-static Comparator comparator_[RES_NUMBER];
-
-/**
- * Locked flag of each resource.  
- */
-static int8 lock_[RES_NUMBER];
+static Comparator cmp_[RES_NUMBER];
 
 /**
  * The driver has been initialized successfully.
@@ -61,45 +101,107 @@ static int8 isInitialized_;
  * Tests if passed value is the resource.
  *
  * @param res an interrupt resource. 
- * @return true if the value if resource.
+ * @return the resource index, or -1 if the resource is wrong.
  */
-static int8 isAlloced(int8 res)
+static int8 getIndex(int8 res)
 {
-  int8 ret, index;
-  ret = 0;
+  int8 index, i;
+  index = -1;
   if(isInitialized_)
   {
     do{
       if( res == 0 ) { break; }    
       if( res & RES_OWNER_MASK != RES_OWNER_MASK ) { break; }          
-      index = res & RES_INDEX_MASK;  
-      if( index < 0 || RES_NUMBER <= index ) { break; }
-      if( lock_[index] == 0 ) { break; }
-      ret = 1;    
+      i = res & RES_INDEX_MASK;  
+      if( i < 0 || RES_NUMBER <= i ) { break; }
+      if( cmp_[i].lock == 0 ) { break; }
+      index = i;
     }while(0);
   }
-  return ret;
+  return index;
+}
+
+/**
+ * Interrupt handler of comparator 0.
+ */
+static void handler0(void)
+{
+  if(cmp_[0].handler.ext != 0)
+  {
+    cmp_[0].handler.ext();
+  }
+}
+
+/**
+ * Interrupt handler of comparator 1.
+ */
+static void handler1(void)
+{
+  if(cmp_[1].handler.ext != 0)
+  {  
+    cmp_[1].handler.ext();  
+  }
 }
 
 /**
  * Returns an comparator resource.
  *
- * @param index available comparator index.
+ * @param handler an interrupt handler function. 
+ * @param index   an available comparator index.
  * @return the comparator resource, or zero if error has been occurred.
  */
-int8 comparatorCreate(int8 index)
+int8 comparatorCreate(void(*handler)(), int8 index)
 {
-  int8 res;
-  res = 0;
-  if( 0 <= index && index < RES_NUMBER )
+  int8 res = 0;  
+  int8 error = BOOS_OK;  
+  if( 0 <= index && index < RES_NUMBER && cmp_[index].lock == 0 )
   {
     do{
-      if(lock_[index] == 1)
-      { 
-        break; 
+      /* Set HW registers */      
+      switch(index)
+      {
+        case 0:
+        {
+          REG_CPT0CN = 0x1 << 7  /* Comparator Enabled */
+                     | 0x0 << 5  /* Comparator Rising Edge has not occurred */
+                     | 0x0 << 4  /* Comparator Falling-Edge has not occurred */
+                     | 0x3 << 2  /* Positive Hysteresis */
+                     | 0x3 << 0; /* Negative Hysteresis */
+          
+          REG_CPT0MD = 0x1 << 7  /* Must Write 1b */
+                     | 0x1 << 5  /* Comparator Rising-edge interrupt enabled */
+                     | 0x1 << 4  /* Comparator Falling-edge interrupt enabled */
+                     | 0x0 << 0; /* Fastest Response Time, Highest Power Consumption */
+          
+          REG_CPT0MX = 0xf << 4  /* Negative Input is Ground */
+                     | 0xf << 0; /* Positive Input is VDD/DC+ Supply Voltage */
+        }
+        break;          
+        case 1:
+        {
+          REG_CPT1CN = 0x1 << 7  /* Comparator Enabled */
+                     | 0x0 << 5  /* Comparator Rising Edge has not occurred */
+                     | 0x0 << 4  /* Comparator Falling-Edge has not occurred */
+                     | 0x3 << 2  /* Positive Hysteresis */
+                     | 0x3 << 0; /* Negative Hysteresis */
+          
+          REG_CPT1MD = 0x1 << 7  /* Must Write 1b */
+                     | 0x1 << 5  /* Comparator Rising-edge interrupt enabled */
+                     | 0x1 << 4  /* Comparator Falling-edge interrupt enabled */
+                     | 0x0 << 0; /* Fastest Response Time, Highest Power Consumption */
+          
+          REG_CPT1MX = 0xf << 4  /* Negative Input is Ground */
+                     | 0xf << 0; /* Positive Input is VDD/DC+ Supply Voltage */          
+        }
+        break;
       }
-      /* Alloc the resource */      
-      lock_[index] = 1;      
+      cmp_[index].inter.res = interruptCreate(cmp_[index].handler.drv, cmp_[index].inter.source);
+      if(cmp_[index].inter.res == 0)
+      {
+        break;
+      }
+      cmp_[index].handler.ext = handler;
+      cmp_[index].lock = 1;
       res = index & RES_INDEX_MASK | RES_OWNER_ID;
     }while(0);
   }
@@ -113,12 +215,101 @@ int8 comparatorCreate(int8 index)
  */
 void comparatorRemove(int8 res)
 {
-  int8 index;
-  if( isAlloced(res) )
+  int8 index = getIndex(res);
+  if( index != -1 )
   {
-    index = res & RES_INDEX_MASK;
-    lock_[index] = 0;
+    switch(index)
+    {
+      case 0:
+      {
+        REG_CPT0CN = 0x1 << 7  /* Comparator Disabled */
+                   | 0x0 << 5  /* Comparator Rising Edge has not occurred */
+                   | 0x0 << 4  /* Comparator Falling-Edge has not occurred */
+                   | 0x0 << 2  /* Positive Hysteresis Disabled */
+                   | 0x0 << 0; /* Negative Hysteresis Disabled */
+      }
+      break;          
+      case 1:
+      {
+        REG_CPT1CN = 0x0 << 7  /* Comparator Disabled */
+                   | 0x0 << 5  /* Comparator Rising Edge has not occurred */
+                   | 0x0 << 4  /* Comparator Falling-Edge has not occurred */
+                   | 0x0 << 2  /* Positive Hysteresis Disabled */
+                   | 0x0 << 0; /* Negative Hysteresis Disabled */
+      }
+      break;
+    }
+    interruptRemove(cmp_[index].inter.res);
+    cmp_[index].handler.ext = 0;    
+    cmp_[index].inter.res = 0;
+    cmp_[index].lock = 0;
   }  
+}
+
+/**
+ * Selects the comparator input channels.
+ *
+ * @param res the comparator resource.
+ * @param neg a negative input channel value.
+ * @param pos a positive input channel value.
+ */
+void comparatorSetInput(int8 res, int8 neg, int8 pos)
+{
+  int8 index = getIndex(res);
+  if( index != -1 )
+  {
+    switch(index)
+    {
+      case 0:
+      {
+        REG_CPT0MX = (neg & 0xf) << 4  /* Negative Input is Ground */
+                   | (pos & 0xf) << 0; /* Positive Input is VDD/DC+ Supply Voltage */
+      }
+      break;          
+      case 1:
+      {
+        REG_CPT1MX = (neg & 0xf) << 4  /* Negative Input is Ground */
+                   | (pos & 0xf) << 0; /* Positive Input is VDD/DC+ Supply Voltage */
+      }
+      break;
+    }    
+  }  
+}
+
+/**
+ * Locks the comparator interrupt source.
+ *
+ * @param res the comparator resource. 
+ * @return an interrupt enable source bit value before function was called.
+ */    
+int8 comparatorIntDisable(int8 res)
+{
+  int8 is;
+  int8 index = getIndex(res);
+  if(index != -1)
+  {
+    is = interruptDisable(cmp_[index].inter.res);
+  }    
+  else
+  {
+    is = 0;
+  }
+  return is;
+}
+    
+/**
+ * Unlocks the comparator interrupt source.
+ *
+ * @param res    the comparator resource. 
+ * @param status status returned by lock function.
+ */
+void comparatorIntEnable(int8 res, int8 status)
+{
+  int8 index = getIndex(res);
+  if(index != -1)
+  {
+    interruptEnable(cmp_[index].inter.res, status);
+  }    
 }
 
 /**
@@ -135,8 +326,29 @@ int8 comparatorInit(void)
   #ifdef BOOS_RESTARTING
   for(i=0; i<RES_NUMBER; i++)
   {
-    lock_[i] = 0;
-    comparator_[i].intRes = 0;
+    cmp_[i].lock = 0;
+    cmp_[i].inter.res = 0;
+    cmp_[i].handler.ext = 0;
+    switch(i)
+    {
+      case 0:
+      {
+        cmp_[i].handler.drv = handler0;            
+        cmp_[i].inter.source = 12;
+      }
+      break;      
+      case 1:
+      {
+        cmp_[i].handler.drv = handler1;            
+        cmp_[i].inter.source = 13;        
+      }
+      break;
+      default: 
+      {
+        break;
+      }
+    }
+    
   }
   /* Disable the comparators */
   REG_CPT0CN = 0x0;
--- boos/include/boos.driver.comparator.h
+++ boos/include/boos.driver.comparator.h
@@ -14,10 +14,11 @@
 /**
  * Returns an comparator resource.
  *
- * @param index available comparator index.
+ * @param handler an interrupt handler function. 
+ * @param index   an available comparator index.
  * @return the comparator resource, or zero if error has been occurred.
  */
-extern int8 comparatorCreate(int8 index);
+extern int8 comparatorCreate(void(*handler)(), int8 index);
 
 /**
  * Removes the comparator resource.
@@ -25,6 +26,31 @@ extern int8 comparatorCreate(int8 index);
  * @param res the comparator resource.
  */
 extern void comparatorRemove(int8 res);
+
+/**
+ * Selects the comparator input channels.
+ *
+ * @param res the comparator resource.
+ * @param neg a negative input channel value.  
+ * @param pos a positive input channel value. 
+ */
+extern void comparatorSetInput(int8 res, int8 neg, int8 pos);
+
+/**
+ * Locks the comparator interrupt source.
+ *
+ * @param res the comparator resource. 
+ * @return an interrupt enable source bit value before function was called.
+ */    
+extern int8 comparatorIntDisable(int8 res);
+    
+/**
+ * Unlocks the comparator interrupt source.
+ *
+ * @param res    the comparator resource. 
+ * @param status status returned by lock function.
+ */
+extern void comparatorIntEnable(int8 res, int8 status);
 
 /**
  * Initializes the driver.
